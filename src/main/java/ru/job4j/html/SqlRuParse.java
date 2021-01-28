81280704e34bd6522c14ddbb32a997cce1bc5ed1
KirillReal <kiril5471710@gmail.com>
2021-01-28 04:38:12 +0300

    2. Парсинг HTML страницы. [#419249]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -2,14 +2,10 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.util.regex.Pattern;
 
 
 public class SqlRuParse {
-   public static final Pattern DATE_PATTERN = Pattern.compile(".*, \\d{2}:\\d{2}");
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
