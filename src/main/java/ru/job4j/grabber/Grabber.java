1b1269e5dcbd2110b14590de4dea337d79a6a534
KirillReal <kiril5471710@gmail.com>
2021-02-04 00:47:51 +0300

    6. Grabber. [#419258]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -20,7 +20,7 @@ public class Grabber implements Grab {
     private final Properties cfg = new Properties();
 
     public Store store() {
-        return null;
+        return new PsqlStore(cfg);
     }
 
     public Scheduler scheduler() throws SchedulerException {
